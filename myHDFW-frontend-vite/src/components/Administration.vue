9c0c23509361e4a0d0bb397416106b4769c37ba2
LukasHennrichs <lukashennrichs@web.de>
2024-01-16 11:41:30 +0100

    added Routerlinks

---
+++ myHDFW-frontend-vite/src/components/Administration.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+    <p>{{ message }}</p>
+  </div>
+  <RouterLink to="/Schedule">Zum Kalender</RouterLink>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hallo',
+      message: 'Willkommen zur Vue 3-Komponente!'
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Hier können Sie den CSS-Stil für Ihre Komponente definieren */
+</style>
