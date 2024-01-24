bb7c3d38b4d5abcbb280753c746470f49562b657
marco <129759421+marco15t@users.noreply.github.com>
2024-01-24 12:24:16 +0100

    Added Sidebar/Header && Updated routing

--- myHDFW-frontend-vite/src/router/index.js
+++ myHDFW-frontend-vite/src/router/index.js
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Schedule from "@/components/Schedule.vue";
 import Administration from "@/components/Administration.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Administration
-    },
-      {
-         path:'/Schedule',
-          name: 'secondPage',
-          component: Schedule
-      }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Administration
+        },
+        {
+            path: '/schedule',
+            name: 'Schedule',
+            component: Schedule
+        }
+    ]
 })
 
 export default router
