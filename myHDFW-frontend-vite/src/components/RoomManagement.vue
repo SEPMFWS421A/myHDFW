e8e79a8e3fb69b242a6291acab3cfee7c6e73414
mffreitag <mikafinn.freitag@mt-itsolutions.com>
2024-02-01 21:53:49 +0100

    Adding styles to page + Adding Design to Dialog Page for editing

--- myHDFW-frontend-vite/src/components/RoomManagement.vue
+++ myHDFW-frontend-vite/src/components/RoomManagement.vue
@@ -9,7 +9,9 @@
           <img id="nav_card_image" class="nav_card_image" alt="user header" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/77f1f60f-2b47-4c08-9c95-df986efe8c7f/dd6yq9v-c9929aee-c4c8-4f67-a54f-44a96a49b8a3.png/v1/fill/w_1024,h_576,q_80,strp/visual_novel_background__classroom_by_kjkjmulo_dd6yq9v-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzc3ZjFmNjBmLTJiNDctNGMwOC05Yzk1LWRmOTg2ZWZlOGM3ZlwvZGQ2eXE5di1jOTkyOWFlZS1jNGM4LTRmNjctYTU0Zi00NGE5NmE0OWI4YTMucG5nIiwiaGVpZ2h0IjoiPD01NzYiLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS53YXRlcm1hcmsiXSwid21rIjp7InBhdGgiOiJcL3dtXC83N2YxZjYwZi0yYjQ3LTRjMDgtOWM5NS1kZjk4NmVmZThjN2ZcL2tqa2ptdWxvLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.tlNsovwcY-weMtUeKVrIOcyOaPdhKWG2SONVOx_dmPE" />
         </RouterLink>
       </template>
-      <template #title> <span class="nav_card_title">Raumverwaltung</span><Button id="add_room" class="help_button" label="" icon="pi pi-question-circle"/>
+      <template #title> 
+        <span class="nav_card_title">Raumverwaltung</span>
+        <Button id="help_button_room" class="help_button" label="" icon="pi pi-question-circle" v-tooltip="{ value: 'Confirm to proceed', showDelay: 1000, hideDelay: 300 }"/>
       </template>
       <template #content>
           <Toolbar>
@@ -43,11 +45,15 @@
           <Column field="capacity" header="Kapazität" sortable style="min-width:16rem"></Column>
           <Column field="exam_capacity" header="Kapazität-Klausur" sortable style="min-width:10rem"></Column>
           <Column field="equipment" header="Ausstattung" sortable style="min-width:10rem"></Column>
-          <Column field="location" header="Standort" sortable style="min-width:10rem"></Column>
+          <Column field="location" header="Standort" sortable style="min-width:10rem">
+            <template #body="slotProps">
+              <Tag :value="slotProps.data.location" />
+            </template>
+          </Column>
           <Column :exportable="false" style="min-width:8rem">
             <template #body="slotProps">
-              <Button style="border-color: #AE5FCF; color: #AE5FCF;" icon="pi pi-pencil" outlined rounded class="mr-2" @click="editRoom(slotProps.data)" />
-              <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteRoom(slotProps.data)" />
+              <Button id="edit_column" class="mr-2 edit_column" icon="pi pi-pencil" outlined rounded @click="editRoom(slotProps.data)" />
+              <Button id="delete_column" class="delete_column" icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteRoom(slotProps.data)" />
             </template>
           </Column>
         </DataTable>
@@ -55,39 +61,74 @@
       
       <!--Dialog zum anlegen-->
       <Dialog v-model:visible="RoomDialog" :style="{width: '450px'}" header="Raum Details" :modal="true" class="p-fluid">
+
         <div class="field">
           <label for="designation">Bezeichnung</label>
-          <InputText id="designation" v-model.trim="Room.designation" required="true" autofocus :class="{'p-invalid': submitted && !Room.designation}" />
+          <InputGroup>
+            <InputGroupAddon>
+                <span class="icons_dialog">abc</span>
+            </InputGroupAddon>
+            <InputText id="column_designation" v-model.trim="Room.designation" required="true" autofocus :class="{'p-invalid': submitted && !Room.designation}" />
+          </InputGroup>
           <small class="p-error" v-if="submitted && !Room.designation">Bezeichnung ist erforderlich!</small>
         </div>
+          
+        <div class="flex gap-3">
+          <div class="field">
+            <label for="capacity">Kapazität</label>
+            <InputGroup>
+              <InputGroupAddon>
+                  <i class="pi pi-building icons_dialog"></i>
+              </InputGroupAddon>
+              <InputText id="column_capacity" v-model.trim="Room.capacity" required="true" autofocus :class="{'p-invalid': submitted && !Room.capacity}" />
+              <small class="p-error" v-if="submitted && !Room.capacity">Kapazität ist erforderlich!</small>
+            </InputGroup>
+          </div>
+  
+          <div class="field">
+            <label for="exam_capacity">Klausurkapazität</label>
+            <InputGroup>
+              <InputGroupAddon>
+                  <i class="pi pi-pencil icons_dialog"></i>
+              </InputGroupAddon>
+              <InputText id="column_exam_capacity" v-model.trim="Room.exam_capacity" required="true" autofocus :class="{'p-invalid': submitted && !Room.exam_capacity}" />
+              <small class="p-error" v-if="submitted && !Room.exam_capacity">Klausurkapazität ist erforderlich!</small>
+            </InputGroup>
+          </div>
+        </div>
 
-        <div class="field">
-          <label for="capacity">Kapazität</label>
-          <InputText id="capacity" v-model.trim="Room.capacity" required="true" autofocus :class="{'p-invalid': submitted && !Room.capacity}" />
-          <small class="p-error" v-if="submitted && !Room.capacity">Kapazität ist erforderlich!</small>
-        </div>
-  
-        <div class="field">
-          <label for="exam_capacity">Klausurkapazität</label>
-          <InputText id="exam_capacity" v-model.trim="Room.exam_capacity" required="true" autofocus :class="{'p-invalid': submitted && !Room.exam_capacity}" />
-          <small class="p-error" v-if="submitted && !Room.exam_capacity">Klausurkapazität ist erforderlich!</small>
-        </div>
-  
         <div class="field">
           <label for="equipment">Ausstattung</label>
-          <InputText id="equipment" v-model.trim="Room.equipment" required="true" autofocus :class="{'p-invalid': submitted && !Room.equipment}" />
-          <small class="p-error" v-if="submitted && !Room.equipment">Ausstattung ist erforderlich!</small>
-        </div>
-  
-        <div class="field">
-          <label for="location">Standort</label>
-          <InputText id="location" v-model.trim="Room.location" required="true" autofocus :class="{'p-invalid': submitted && !Room.location}" />
-          <small class="p-error" v-if="submitted && !Room.location">Standort ist erforderlich!</small>
+          <InputGroup>
+            <InputGroupAddon>
+                <i class="pi pi-info icons_dialog"></i>
+            </InputGroupAddon>
+            <InputText id="equipment" v-model.trim="Room.equipment" required="true" autofocus :class="{'p-invalid': submitted && !Room.equipment}" />
+            <small class="p-error" v-if="submitted && !Room.equipment">Ausstattung ist erforderlich!</small>
+          </InputGroup>
         </div>
 
+        <div class="field" id="field_location">
+          <label for="location">Standort</label>
+          <Dropdown id="location" v-model="Room.location" editable :options="locations" optionLabel="name" placeholder="Suche oder wähle einen Standort aus" class="w-full md:w-14rem" />
+        </div>
+
+        <!--
+        <div v-for="category in categories" :key="category.key" class="flex align-items-center">
+            <RadioButton v-model="selectedCategory" :inputId="category.key" name="dynamic" :value="category.name" />
+            <label :for="category.key" class="ml-2">{{ category.name }}</label>
+        </div>
+
+        <div class="field">
+          <label for="location">Standort</label>
+          <InputText id="column_location" v-model.trim="Room.location" required="true" autofocus :class="{'p-invalid': submitted && !Room.location}" />
+          <small class="p-error" v-if="submitted && !Room.location">Standort ist erforderlich!</small>
+        </div>
+        -->
+
         <template #footer>
-          <Button id="cancel_add_room" label="Cancel" icon="pi pi-times" text @click="hideDialog"/>
-          <Button id="add_room" label="Save" icon="pi pi-check" text @click="saveRoom" />
+          <Button id="cancel_add_room" class="cancel_dialog" label="Cancel" icon="pi pi-times" text @click="hideDialog"/>
+          <Button id="add_room" class="save_dialog" label="Save" icon="pi pi-check" text @click="saveRoom" />
         </template>
       </Dialog>
   
@@ -142,6 +183,12 @@
     'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
   });
   const submitted = ref(false);
+
+  const locations = ref([
+    { name: 'Mettmann'},
+    { name: 'Paderborn'},
+    { name: 'Bergisch-Gladbach'}
+]);
 
   const openNew = () => {
     Room.value = {};
@@ -219,5 +266,10 @@
     selectedRooms.value = null;
     toast.add({severity:'success', summary: 'Successful', detail: 'Rooms Deleted', life: 3000});
   };
-  
   </script>
+
+  <style scoped>
+    #location{
+      width: 100% !important;
+    }
+  </style>
